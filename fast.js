d976e68201f029b1550d2b54f91ec58d463b8d66
blacdolla007 <105276386+blacdolla007@users.noreply.github.com>
2022-06-23 14:40:51 +0300

    Add files via upload

---
+++ fast.js
@@ -0,0 +1,14 @@
+function print(){
+    alert("My name is KENNEDY KITAVI")
+    alert("I am 2 metres tall")
+    alert("and I live in Kenya")
+}
+
+print()
+
+function screen(){
+    document.write("My name is kennedy kitavi")
+    document.write(" I am 2 metres tall")
+    document.write(" And I live in Kenya")
+}
+
